35ed67a2718cefb2d0a061703f6e35aa2846c059
VoicuStefan2001 <150918937+VoicuStefan2001@users.noreply.github.com>
2023-11-21 09:50:01 +0200

    Add files via upload

---
+++ om_hospital/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name':'Hospital Management',
+    'author':'Voicu Stefan',
+    'summary':'Odoo 15 Hospital Module',
+    'data':[
+        'views/menu.xml',
+        'views/patient.xml',
+    ]
+}
